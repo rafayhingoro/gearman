d6a36475ba9d0db21c96e695fd8b2a6cec9416c0
Rafay Hingoro <rafayhingoro@hotmail.com>
2020-05-12 13:10:58 +0400

    multi-tasks

--- index.php
+++ index.php
@@ -4,4 +4,15 @@ ini_set('display_errors', -1);
 $client = new GearmanClient();
 $client->addServer();
 
-echo $client->do('random', 'boomerang');
+$client->setCompleteCallback('completed');
+
+// multiple tasks assignment
+for( $i = 0; $i < 5; $i++) {
+    $client->addTask('random', 'boomerang', null, $i);
+}
+
+function completed(GearmanTask $task) {
+    echo $task->data() .' - '. $task->unique() ."\n<br>"; 
+}
+
+$client->runTasks();
--- server.php
+++ server.php
@@ -10,9 +10,22 @@ $server = new CustomServer;
 
 // add some work
 $a = $server->addWork('random', function($job){
-    $fn = ['strrev', 'strtoupper', 'strtolower', 'str_shuffle'];
+    $fn = ['bin2hex', 'strrev', 'strtoupper', 'strtolower', 'str_shuffle'];
     shuffle($fn);
     return call_user_func($fn[0], $job->workload());
+});
+
+// add some more work
+$a = $server->addWork('randomtoken', function($job){
+    if( is_string($data) && $data = unserialize( $job->workload() ) ) {
+        if(isset($data['hash']) && isset($data['hashtype']) && isset($data['string'])) {
+            return hash($data['hashtype'], $data['string']);
+        } else {
+            return hash('sha1', rand(0000,9999));
+        }
+    } else {
+        return rand(0000, 9999);
+    }
 });
 
 // execute server
